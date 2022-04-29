f9d4335b669b0833500775b8e1884b31e2c2901a
Geuni <dlrmsgnl620@naver.com>
2022-04-29 15:24:09 +0900

    Add: Nav 레이아웃 초안 완료

---
+++ src/components/ChannelSearch/ChannelSearch.js
@@ -0,0 +1,146 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useChatContext } from 'stream-chat-react';
+import _debounce from 'lodash.debounce';
+
+import './ChannelSearch.css';
+
+import { ResultsDropdown } from './ResultsDropdown';
+import SearchIcon from '../../assets/SearchIcon';
+
+const ChannelSearch = () => {
+  const { client, setActiveChannel } = useChatContext();
+
+  const [allChannels, setAllChannels] = useState([]);
+  const [teamChannels, setTeamChannels] = useState([]);
+  const [directChannels, setDirectChannels] = useState([]);
+
+  const [focused, setFocused] = useState(undefined);
+  const [focusedId, setFocusedId] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState('');
+
+  const handleKeyDown = useCallback(
+    event => {
+      if (event.key === 'ArrowDown') {
+        setFocused(prevFocused => {
+          if (prevFocused === undefined) return 0;
+          return prevFocused === allChannels.length - 1 ? 0 : prevFocused + 1;
+        });
+      } else if (event.key === 'ArrowUp') {
+        setFocused(prevFocused => {
+          if (prevFocused === undefined) return 0;
+          return prevFocused === 0 ? allChannels.length - 1 : prevFocused - 1;
+        });
+      } else if (event.keyCode === 13) {
+        event.preventDefault();
+        setActiveChannel(allChannels[focused]);
+        setFocused(undefined);
+        setFocusedId('');
+        setQuery('');
+      }
+    },
+    [allChannels, focused, setActiveChannel]
+  );
+
+  useEffect(() => {
+    if (query) {
+      document.addEventListener('keydown', handleKeyDown);
+    }
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, [handleKeyDown, query]);
+
+  useEffect(() => {
+    if (!query) {
+      setTeamChannels([]);
+      setDirectChannels([]);
+    }
+  }, [query]);
+
+  useEffect(() => {
+    if (focused >= 0) {
+      setFocusedId(allChannels[focused]?.id);
+    }
+  }, [allChannels, focused]);
+
+  const setChannel = channel => {
+    setQuery('');
+    setActiveChannel(channel);
+  };
+
+  const getChannels = async text => {
+    try {
+      const channelResponse = client.queryChannels(
+        {
+          type: 'team',
+          name: { $autocomplete: text },
+        },
+        {},
+        { limit: 5 }
+      );
+
+      const userResponse = client.queryUsers(
+        {
+          id: { $ne: client.userID },
+          $and: [{ name: { $autocomplete: text } }],
+        },
+        { id: 1 },
+        { limit: 5 }
+      );
+
+      const [channels, { users }] = await Promise.all([
+        channelResponse,
+        userResponse,
+      ]);
+
+      if (channels.length) setTeamChannels(channels);
+      if (users.length) setDirectChannels(users);
+      setAllChannels(channels.concat(users));
+    } catch (e) {
+      setQuery('');
+      console.log(e);
+    }
+
+    setLoading(false);
+  };
+
+  const getChannelsDebounce = _debounce(getChannels, 100, {
+    trailing: true,
+  });
+
+  const onSearch = event => {
+    event.preventDefault();
+
+    setLoading(true);
+    setFocused(undefined);
+    setQuery(event.target.value);
+    if (!event.target.value) return;
+
+    getChannelsDebounce(event.target.value);
+  };
+
+  return (
+    <div className="channel-search__container">
+      <input
+        className="channel-search__input__text"
+        onChange={onSearch}
+        placeholder="Search people, company, message"
+        type="text"
+        value={query}
+      />
+      <SearchIcon />
+
+      {query && (
+        <ResultsDropdown
+          teamChannels={teamChannels}
+          directChannels={directChannels}
+          focusedId={focusedId}
+          loading={loading}
+          setChannel={setChannel}
+          setQuery={setQuery}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ChannelSearch;
